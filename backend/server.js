1043220b097da127ceb7103dce51605a1e3a6a54
lordbomebob <lordbomebob@gmail.com>
2024-07-30 23:15:35 -0700

    add more package, server works with router

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 const app = express()
 const port = process.env.port || 5000
-
+const path=require(`path`)
 app.use(cors())
 app.use(express.json())
 
@@ -16,6 +16,12 @@ const connection= mongoose.connection
 connection.once(`open`, ()=>{
     console.log(`mongodb database connection established successfully`)
 })
+
+const userRouter= require(`./routes/user.route`)
+//const storyRouter= require(`./routes/story`)
+app.use(express.static(path.join(__dirname,`frontend`)))
+app.use(`/user`, userRouter)
+//app.use(`/story`, storyRouter)
 
 app.listen(port,()=>{
     console.log(`server is running on prot: ${port}`)
