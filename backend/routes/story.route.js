ddc2ff78645709007b1ac61c558ea03573385884
lordbomebob <lordbomebob@gmail.com>
2024-08-19 00:28:37 -0700

    the post getChapter is suppose to be get but limited by axios so must stay as post

--- backend/routes/story.route.js
+++ backend/routes/story.route.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 let story = require('../models/story.model');
 let chapter = require(`../models/chapter.model`)
 const storyController = require(`../controller/story.controller`)
+//get story
 router.get(`/`,storyController.getStory)
 
 //post story
 router.post('/tell',storyController.storyBeginning)
 //post chapter
 router.post(`/tell/chapter`,storyController.addChapter)
-
+//get chapter based on title
+router.post('/listen',storyController.getChapters)
 
 module.exports = router;
